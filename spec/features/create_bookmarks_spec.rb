b36a08d62b95a4f46911503db9a9a6aecaee7b50
Daniel Mortensen <daniel.j.mortensen@gmail.com>
2014-09-11 19:58:03 +0100

    added bookmarks scaffold

---
+++ spec/features/create_bookmarks_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+#As a user
+#I want to be able to save a bookmark
+#So that I can refer to it later
+
+feature 'create bookmark' do
+	
+	scenario 'successfully' do
+		visit bookmarks_path
+		click_link 'New Bookmark'
+		fill_in 'Title', with: 'New Bookmark'
+		fill_in 'Url', with: 'http://www.coolwebsite.com'
+		click_button 'Create Bookmark'
+
+		expect(page).to have_content('Bookmark was successfully created')
+	end
+
+end
