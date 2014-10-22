9c36718a6bc9707ff068c5abbb11e322728d20f6
Scotty Runyan <runyans7@gmail.com>
2014-10-22 16:48:10 -0400

    Resolve merge conflict

--- spec/features/favoriting_bookmarks_spec.rb
+++ spec/features/favoriting_bookmarks_spec.rb
@@ -8,8 +8,20 @@ feature 'create favorite' do
   scenario 'favorite a bookmark' do
     user = create(:user)
     bookmark = create(:bookmark, )
+    #User logs in
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content('Signed in successfully')
 
-		#Expect page to have 'Favorited'
-		expect(page).to have_content('Unfavorite')	
-	end
+    #User goes to bookmark_show
+    visit bookmarks_path
+    click_link "#{bookmark.title}"
+
+    #User clicks on Favorite
+    click_link 'Favorite'
+    expect(page).to have_content('Unfavorite')	
+
+  end
 end
